4f3e62d87a9101e393041ced6fe6b2506e98d843
Joni Koivula <joni.koivula@student.hamk.fi>
2017-01-28 14:21:58 +0200

    ulkoasua viimeistelty, profiilikuva ja teksti lisätty

--- app.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope, $http) {
-    $http.get('projektit.json').success(function(data){
-      $scope.projektit = data;
-    });
-});
